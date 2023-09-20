b0e33cf1655fba6a5b27c9d2fbb51c2b86a03c63
Teofanis Papadopulos <teos_97@hotmail.com>
2023-09-20 18:53:49 +0100

    fix(`.eslint.cjs`): fix eslint

--- packages/core/.eslintrc.cjs
+++ packages/core/.eslintrc.cjs
@@ -2,6 +2,6 @@ module.exports = {
   root: true,
   extends: ['@composite-fetcher/eslint-config/base'],
   parserOptions: {
-    project: ['packages/core/tsconfig.json', './tsconfig.json'],
+    project: ['./tsconfig.json'],
   },
 };
